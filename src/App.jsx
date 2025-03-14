fb5149d5c85deed7079d49f657c5f70e70f3b733
DiegoBas17 <d.basili17@gmail.com>
2025-03-14 16:19:11 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Home from './components/home'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./components/home/Home";
 
 function App() {
-
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-    <Route path="/" element={<Home />} />
-    </Routes> 
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
