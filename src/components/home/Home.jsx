90edb18bb3904a0eda45623592e70cc49f69068e
DiegoBas17 <d.basili17@gmail.com>
2025-03-14 15:56:55 +0100

    Initial commit

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="bg-pink-500 text-white text-3xl font-bold underline p-6">
+        Questo è il sito per Bellazza Permanente di Erica Bianchini
+      </div>
+    </>
+  );
+};
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{html,js,jsx,ts,tsx}'],  // Aggiungi il percorso per i tuoi file
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
